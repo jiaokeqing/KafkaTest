aa6f028acea62e701e194b41c83b27ceda3f59df
jiaokeqing <13583243451@163.com>
2021-07-20 14:54:00 +0800

    Initial commit

---
+++ src/main/java/com/test/Producer.java
@@ -0,0 +1,40 @@
+package com.test;
+
+
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+/**
+ * 生产者
+ * @author jkq
+ */
+public class Producer {
+    public static String topic = "duanjt_test";//定义主题
+
+    public static void main(String[] args) throws InterruptedException {
+        Properties p = new Properties();
+        //kafka地址，多个地址用逗号分割
+        p.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.100.141:9092");
+        p.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        p.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(p);
+
+        try {
+            while (true) {
+                String msg = "Hello," + new Random().nextInt(100);
+                ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, msg);
+                kafkaProducer.send(record);
+                System.out.println("消息发送成功:" + msg);
+                Thread.sleep(500);
+            }
+        } finally {
+            kafkaProducer.close();
+        }
+
+    }
+}
